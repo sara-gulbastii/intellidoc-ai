31025a2add5717beb1481d59f6e9598ee7246117
Sarah Maryam <saragulbasti@gmail.com>
2026-01-03 16:58:01 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['pdfjs-dist/build/pdf.worker.entry'],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['pdfjs-dist'],
+  },
 });
